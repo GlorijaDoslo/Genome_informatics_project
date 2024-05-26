53435f705bcb201bbd464b1eb139c115c490a0cc
jovanannikolic44 <jovana.nikolic452@gmail.com>
2024-05-26 15:31:04 +0200

    Add comments in the functions

--- optimizations.py
+++ optimizations.py
@@ -1,6 +1,22 @@
 from burrowsWheeler import first_column
 
 def fm_search_optimized(bwt_string, ranks, tots, first_col_positions, suffix_array, suffix_array_checkpoint=1):
+    """
+    Optimized search for a pattern using the found pattern positions in the first column and suffix array
+    with checkpoints to get indexes of pattern in original text.
+
+    Parameters:
+        bwt_string (str): The last column of BWT matrix.
+        ranks (list): The ranks list calculated from BWT.
+        tots (dict): The total counts of characters in BWT.
+        first_col_positions (list): The range of pattern occurrences in first column.
+        suffix_array (list): Suffix array of the original text.
+        suffix_array_checkpoint (int): Checkpoint where values are found.
+    
+    Returns:
+        indexes (list): Indexes of the pattern in the original text.
+    """
+
     if suffix_array_checkpoint < 1:
         print("Not allowed value for suffix array checkpoint.")
         return -1
@@ -24,8 +40,18 @@ def fm_search_optimized(bwt_string, ranks, tots, first_col_positions, suffix_arr
 
     return indexes
 
-# optimize already created suffix array - save just checkpoints
 def suffix_array_optimized(suffix_array, suffix_array_checkpoint=1):
+    """
+    Creates optimized suffix array with checkpoints.
+
+    Parameters:
+        suffix_array (list): Suffix array of the original text.
+        suffix_array_checkpoint (int): Checkpoint where values are found.
+        
+    Returns:
+        suffix_array_opt (list): Optimized suffix array which is containing only values in checkpoints.
+    """
+
     if suffix_array_checkpoint < 1:
         print("Not allowed value for suffix array checkpoint.")
         return -1
@@ -40,8 +66,20 @@ def suffix_array_optimized(suffix_array, suffix_array_checkpoint=1):
 
     return suffix_array_opt
 
-# returns position of pattern in first column
 def find_pattern_position_in_first(bwt_string, pattern, tots, tally_checkpoint=1):
+    """
+    Finds range where pattern occurs in first column of BWT matrix.
+        
+    Parameters:
+        bwt_string (str): The last column of BWT matrix.
+        pattern (str): Pattern to be found.
+        tots (dict): The total counts of characters in BWT.
+        tally_checkpoint (int): Checkpoint where values are found.
+
+    Returns:
+        first_col_positions (list): The range of pattern occurrences in the first column.
+    """
+
     if not pattern or tally_checkpoint < 0:
         print("No pattern or invalid value for tally checkpoint (must be >= 1).")
         return []
@@ -64,10 +102,8 @@ def find_pattern_position_in_first(bwt_string, pattern, tots, tally_checkpoint=1
         char_to_find = pattern[i]
 
         if lower_index_ex % tally_checkpoint == 0:
-            # rank can be found in tally since in it is the index of checkpoint
             lower_rank = tally[char_to_find][lower_index_ex // tally_checkpoint]
         else:
-            # find rank using the checkpoint
             index, count = find_occurrences_to_the_checkpoint(bwt_string, lower_index_ex, char_to_find, tally_checkpoint)
             lower_rank = tally[char_to_find][index // tally_checkpoint] + count
 
@@ -82,16 +118,28 @@ def find_pattern_position_in_first(bwt_string, pattern, tots, tally_checkpoint=1
             print("Pattern not found.")
             return []
 
-        # since ranks are found for last column, we search it in the first column, so
-        # we need to set lower_index_ex and upper_index_incl to correspond the char in the first column
         lower_index_ex = first_col[char_to_find][0] + lower_rank - 1
         upper_index_incl = first_col[char_to_find][0] + lower_rank + char_occurences_num - 1
 
-    return [lower_index_ex + 1, upper_index_incl]
+    first_col_positions = [lower_index_ex + 1, upper_index_incl]
+    return first_col_positions
 
-
-# calculate how much char_to_find characters are found in the way to the checkpoint
 def find_occurrences_to_the_checkpoint(bwt_string, limit_index, char_to_find, tally_checkpoint):
+    """
+    Called when certain character is not in tally matrix. Finds that character in BWT last column 
+    and counts the occurrences of it. Counting stops when the tally checkpoint is reached.
+
+    Parameters:
+        bwt_string (str): The last column of BWT matrix.
+        limit_index (int): Starting index for character search.
+        char_to_find (str): Character to be found.
+        tally_checkpoint (int): Checkpoint where values are found.
+        
+    Returns:
+        current_index (int): Index of found character in the checkpoint.
+        count (int): Number of occurrences of the character in the way to the checkpoint.
+    """
+
     count = 0
     current_index = limit_index
 
@@ -105,20 +153,26 @@ def find_occurrences_to_the_checkpoint(bwt_string, limit_index, char_to_find, ta
 
 
 def create_tally(bwt_string, tally_checkpoint):
+    """
+    Creates tally matrix.
+
+    Parameters:
+        bwt_string (str): The last column of BWT matrix.
+        tally_checkpoint(int): Checkpoint where values are found.
+
+    Returns:
+        tally (dict): Created tally matrix.
+    """
+
     char_occurrences = {}
     tally = {}
 
-    # initialize
     for char in bwt_string:
         if char != '$':
             if char not in char_occurrences:
                 char_occurrences[char] = 0
                 tally[char] = []
 
-    # char_occurences contains current count number for each character
-    # not all rows from tally are saved, it depends on checkpoint length
-    # since we are not storing all tally rows, char_occurrences is needed to save count of not stored values
-    # so we can use it to calculate the next checkpoints
     for index, char in enumerate(bwt_string):
         if char != '$':
             char_occurrences[char] += 1
@@ -126,7 +180,6 @@ def create_tally(bwt_string, tally_checkpoint):
                 for char in tally.keys():
                     tally[char].append(char_occurrences[char])
         elif char == '$':
-            # if char is $, add occurrences row for it
             if index % tally_checkpoint == 0:
                 for char in tally.keys():
                     tally[char].append(char_occurrences[char])
