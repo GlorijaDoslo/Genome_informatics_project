9a8a739ead2989377acd4f8d47c2ef3c725ec7b8
jovanannikolic44 <jovana.nikolic452@gmail.com>
2024-05-27 10:16:33 +0200

    Change for loop and remove unnecessary memory optimization code

--- app.py
+++ app.py
@@ -19,48 +19,48 @@ tally_checkpoint = args.tally_checkpoint if args.tally_checkpoint is not None el
 suffix_array_checkpoint = args.suffix_array_checkpoint if args.suffix_array_checkpoint is not None else 1
 
 
-for file_name in file_names:
+for file_name, file_patterns in zip(file_names, ALL_PATTERNS):
     print(f"File: {file_name}")
+
+    suffix_array_init = None
+    suffix_array_opt = None
+    tally = None
+
     sequences_from_fasta = read_fasta(file_name)
     sequences_from_fasta_concatenated = ''.join(sequences_from_fasta)
 
-    for file_patterns in ALL_PATTERNS:
-        for pattern in file_patterns:
-            print(f"Pattern: {pattern}")
-            suffix_array_init = suffix_array(sequences_from_fasta_concatenated)
-            bwt_text = bwt(sequences_from_fasta_concatenated, suffix_array_init)
-            ranks, tots = rank_bwt(bwt_text)
+    for pattern in file_patterns:
+        print(f"Pattern: {pattern}")
+        suffix_array_init = suffix_array(sequences_from_fasta_concatenated)
+        bwt_text = bwt(sequences_from_fasta_concatenated, suffix_array_init)
+        ranks, tots = rank_bwt(bwt_text)
 
-            # TIME
-            # time for search without optimizations
-            start_search_time = time.process_time()
-            find_sequence = index_search(bwt_text, pattern, suffix_array_init, ranks, tots)
-            end_search_time = time.process_time()
-            search_time = end_search_time - start_search_time
-            print(f"Search time without optimizations: {search_time} seconds")
+        # TIME
+        # time for search without optimizations
+        start_search_time = time.process_time()
+        find_sequence = index_search(bwt_text, pattern, suffix_array_init, ranks, tots)
+        end_search_time = time.process_time()
+        search_time = end_search_time - start_search_time
+        print(f"Search time without optimizations: {search_time} seconds")
 
-            tally, suffix_array_opt = precalculate_tally_and_suffix_array(bwt_text, tally_checkpoint, suffix_array_init, suffix_array_checkpoint)
+        tally, suffix_array_opt = precalculate_tally_and_suffix_array(bwt_text, tally_checkpoint, suffix_array_init, suffix_array_checkpoint)
 
-            # time for optimized search
-            search_time_start = time.process_time()
-            find_sequence = fm_search_optimized(bwt_text, pattern, ranks, tots, tally, tally_checkpoint, suffix_array_opt, suffix_array_checkpoint)
-            search_time_end = time.process_time()
-            search_time = search_time_end - search_time_start
-            print(f"Search time with optimizations: {search_time} seconds")
+        # time for optimized search
+        search_time_start = time.process_time()
+        find_sequence = fm_search_optimized(bwt_text, pattern, ranks, tots, tally, tally_checkpoint, suffix_array_opt, suffix_array_checkpoint)
+        search_time_end = time.process_time()
+        search_time = search_time_end - search_time_start
+        print(f"Search time with optimizations: {search_time} seconds")
 
-            # MEMORY - can be outside of for - same for all patterns in one file
-            # memory usage without optimization [bwt, rank and suffix array]
-            suffix_array_init_memory = asizeof.asizeof(suffix_array_init)
-            # bwt_memory = asizeof.asizeof(bwt_text)
-            # ranks_memory = asizeof.asizeof(ranks)
-            # memory_without_optimization = (bwt_memory + ranks_memory + suffix_array_memory) / (1024 * 1024)
-            memory_without_optimization = (suffix_array_init_memory) / (1024 * 1024)
-            print(f"Memory usage without optimization: {memory_without_optimization} MB")
 
-            # memory usage with optimization [bwt, rank, tally matrix and suffix array optimized]
-            suffix_array_optimized_memory = asizeof.asizeof(suffix_array_opt)
-            tally_memory = asizeof.asizeof(tally)
-            # memory_with_optimization = (bwt_memory + ranks_memory + tally_memory + suffix_array_optimized_memory) / (1024 * 1024)
-            memory_with_optimization = (tally_memory + suffix_array_optimized_memory) / (1024 * 1024)
-            print(f"Memory usage with optimization: {memory_with_optimization} MB")
-            
+    # memory usage without optimization
+    suffix_array_init_memory = asizeof.asizeof(suffix_array_init)
+    memory_without_optimization = (suffix_array_init_memory) / (1024 * 1024)
+    print(f"Memory usage without optimization: {memory_without_optimization} MB")
+
+    # memory_with_optimization
+    suffix_array_optimized_memory = asizeof.asizeof(suffix_array_opt)
+    tally_memory = asizeof.asizeof(tally)
+    memory_with_optimization = (tally_memory + suffix_array_optimized_memory) / (1024 * 1024)
+    print(f"Memory usage with optimization: {memory_with_optimization} MB")
+    
